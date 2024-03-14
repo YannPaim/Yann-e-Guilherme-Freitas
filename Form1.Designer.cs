842227b03e64f31aac95ca13bd89e9afb75a365b
YannPaim <161754380+YannPaim@users.noreply.github.com>
2024-03-14 16:47:47 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Uiui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textSenha = new System.Windows.Forms.Label();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(314, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textSenha
+            // 
+            this.textSenha.AutoSize = true;
+            this.textSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSenha.Location = new System.Drawing.Point(294, 221);
+            this.textSenha.Name = "textSenha";
+            this.textSenha.Size = new System.Drawing.Size(106, 25);
+            this.textSenha.TabIndex = 2;
+            this.textSenha.Text = "Password";
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(299, 179);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(160, 20);
+            this.textUser.TabIndex = 3;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(299, 249);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(160, 20);
+            this.textPassword.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(309, 353);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(30, 25);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "...";
+            this.lblAnswer.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.textSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label textSenha;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uiui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = textPassword.Text;
+            string user = textUser.Text;
+
+            if ( password =="ds@etefmc" && user == "admin")
+            {
+                MessageBox.Show("Login confirmado!");
+            }
+            else
+            {
+                lblAnswer.Text = "Login ou senha incorreto, tente novamente";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
